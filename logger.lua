ff1888f15fa10d7d805c9cb968611f23b3f17fd5
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2024-11-30 08:28:50 +0500

    Create logger.lua

---
+++ logger.lua
@@ -0,0 +1,198 @@
+((PROTECTED W RG));
+ *D#*&d8gd#8GD#d*#&d*&GD@&265f@7528d7n2D&Fdf8ndgwgnwygccg7y'
+wDcwc
+4L<":<",$
+"C<:$
+'c4,rc"
+$<C$"
+<:4 .4'
+>'. 
+RC4'. 
+c$C 4'.
+CCCCQCRwq'r C>w 'c
+>wqc
+'qc>
+cw 'wqC
+WQ'c'rcw'W>'wRC
+'qw'. r .rc .
+ rC "
+wrCQ" 
+wCQR '
+wCQR
+ .r
+ .cr
+ '.crQ
+' .r
+ .rqq'r
+ wr '
+rq'
+. rcqw
+' .rrwq
+ '.rq 'CR'
+ r.qw q 'QCq'
+.  '.Ww '
+' .
+  > E > >"E ".e e E"   "
+"E>e  >"
+EQQ >WQ
+ .WQ '.QQ
+E ">E >QE
+Q" >EE >E >'eQ 
+.'eq
+ .'eq   .
+q'e .
+e  q'eq >
+"Q >"eq
+ .'eq
+. 'e
+e ." >  q >"
+eq .
+'  eQ .
+'  eq
+ .'eq
+ .'  eQe  Q 
+>'eq
+ ">Q
+ ">e
+Q ">  q
+>" e  Q
+ ">e   q
+">  eq 
+">e  q
+ ">e q
+  .'
+ .q'     q
+">  
+ q.' 
+' .d.'qe  d
+.'
+ .q'eq
+" 
+'EQDe' eqd 
+'qeded .'eqd '.D 'EQD 'dEQ 'deQ'deq 'deq ' d' ed' de  q'd  e
+d eqe      'eqd 'eqd 'qqdeq' 
+   ed
+'   ed q de
+ edq
+ ed  q edq edq
+ qd
+   qed
+ deq
+ deq
+ deq  
+ deq d q
+ q e
+q
+e d
+ ed;ee
+;.e qeedq; qE"D":ed.';e.;e.q;EQD.;EDQ
+eqdEQD
+EDEQ
+
+
+"'?Ed
+dee/'D
+eded?"
+dq/'qed
+?"de
+'/ed'D?d
+>:DE.;DE>:DE?D;DEd;E
+;?DE;/ED
+:?DE
+;Ed;E>D;ED
+
+/'''/'/eQ'/
+'/
+e"ed
+q?"edq
+'?de
+'?De
+?"ed
+'?De
+D
+';.
+'.;  QA
+;.'  qA
+>:"  a
+;q'A
+":QAqa
+":  qa
+;.'q  A
+:>"q  
+a:>"
+.;'  qa
+>:"Q  A
+:">Q
+a';.
+q.;a
+';  .qa
+>":qa
+:>"A
+:>"Q
+A":>  
+
+QQ        
+.;"Q
+:>"Q
+:>'q
+;.'Q
+.;q
+.;  Q
+:>  q
+>:
+.;:>
+>:w
+;.qw
+.;  
+;.qw
+.;'  qw.;  q
+.;w
+>Q:W
+>:qW
+>:  q
+.w;
+:>QW
+>:Q
+:W
+.  ;qW
+>:  
+;.w;
+.  q;
+.q
+;.						
+
+?
+./	q/
+q
+/.q/.q
+/qq
+/.q	/.
+q	./
+q	/.qq
+/qq	/q	.q.q.';q
+.;'q.q
+;	'q	.
+;'q	
+.;'q	
+.;q	
+.q;	q	.;
+q	.;
+q	
+.;q	.;
+q	;
+.q	.;
+q
+.;	q;
+.Q
+;.q	
+;.Q.;q.;q.;.';	q.';	q>:"Q>:
+"	q
+>:'Q>;'q	
+	?"
+/"	q
+?"q
+'?q
+?"q
+/'Qq
+/"	q
+?"
